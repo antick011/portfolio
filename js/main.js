58dfaee19b334db833127a5336e5f935923da5de
antick011 <antickbh15122001@gmail.com>
2025-04-12 17:12:47 +0530

    Uploading Portfolio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Optional: Smooth scroll or any custom JavaScript
+document.querySelectorAll('a.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+      const navbar = document.querySelector('.navbar-collapse');
+      if (navbar.classList.contains('show')) {
+        new bootstrap.Collapse(navbar).toggle();
+      }
+    });
+  });
+  
